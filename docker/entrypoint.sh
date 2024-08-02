90b6a341fa46934c93a17e63f7f75ed19396f889
Thao Dang <thao.dang@hs-esslingen.de>
2024-08-02 23:07:30 +0200

    fixed user rights

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,20 +2,20 @@
 set -e
 
 # colcon
+HOME=/home/vscode
 BASHRC_PATH=$HOME/.bashrc
 grep -F "source /opt/ros/$ROS_DISTRO/setup.bash" $BASHRC_PATH || echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> $BASHRC_PATH
 grep -F "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" $BASHRC_PATH || echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> $BASHRC_PATH
-chown $USER:$USER $BASHRC_PATH
+chown vscode:vscode $BASHRC_PATH
 
 # Fix rosdep permission
 mkdir -p $HOME/.ros
 cp -r /root/.ros/rosdep $HOME/.ros/rosdep
-chown -R $USER:$USER $HOME/.ros
+chown -R vscode:vscode $HOME/.ros
 
 source /opt/ros/$ROS_DISTRO/setup.bash
 
+mkdir -p /workspace/src
 sudo chown vscode:vscode /workspace /workspace/src
-
-mkdir -p /workspace/src && ([ -f /workspace/src/CMakeLists.txt ] || catkin_init_workspace /workspace/src)
 
 exec "$@"
