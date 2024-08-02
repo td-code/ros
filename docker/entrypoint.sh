38e1b4b58dd7980d376ecd4728c1047504f57515
Thao Dang <thao.dang@hs-esslingen.de>
2024-08-02 21:46:23 +0200

    ros2 initial and untested

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+# colcon
+BASHRC_PATH=$HOME/.bashrc
+grep -F "source /opt/ros/$ROS_DISTRO/setup.bash" $BASHRC_PATH || echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> $BASHRC_PATH
+grep -F "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" $BASHRC_PATH || echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> $BASHRC_PATH
+chown $USER:$USER $BASHRC_PATH
+
+# Fix rosdep permission
+mkdir -p $HOME/.ros
+cp -r /root/.ros/rosdep $HOME/.ros/rosdep
+chown -R $USER:$USER $HOME/.ros
+
 source /opt/ros/$ROS_DISTRO/setup.bash
 
 sudo chown vscode:vscode /workspace /workspace/src
--- docker/library-scripts/desktop-lite-debian.sh
+++ docker/library-scripts/desktop-lite-debian.sh
@@ -4,16 +4,23 @@
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
 #-------------------------------------------------------------------------------------------------------------
 #
-# Docs: https://github.com/microsoft/vscode-dev-containers/blob/master/script-library/docs/desktop-lite.md
+# Docs: https://github.com/microsoft/vscode-dev-containers/blob/main/script-library/docs/desktop-lite.md
 # Maintainer: The VS Code and Codespaces Teams
-#
-# Syntax: ./desktop-lite-debian.sh [non-root user] [vnc password] [install no vnc flag]
 
-USERNAME=${1:-"automatic"}
-VNC_PASSWORD=${2:-"vscode"}
-INSTALL_NOVNC=${3:-"true"}
+NOVNC_VERSION="${NOVNCVERSION:-"1.2.0"}" # TODO: Add in a 'latest' auto-detect and swap name to 'version'
+VNC_PASSWORD=${PASSWORD:-"vscode"}
+if [ "$VNC_PASSWORD" = "noPassword" ]; then
+    unset VNC_PASSWORD
+fi
+NOVNC_PORT="${WEBPORT:-6080}"
+VNC_PORT="${VNCPORT:-5901}"
 
-PACKAGE_LIST="
+INSTALL_NOVNC="${INSTALL_NOVNC:-"true"}"
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
+
+WEBSOCKETIFY_VERSION=0.10.0
+
+package_list="
     tigervnc-standalone-server \
     tigervnc-common \
     fluxbox \
@@ -25,7 +32,6 @@ PACKAGE_LIST="
     at-spi2-core \
     xterm \
     eterm \
-    tilix \
     nautilus\
     mousepad \
     seahorse \
@@ -38,7 +44,6 @@ PACKAGE_LIST="
     libnotify4 \
     libnss3 \
     libxss1 \
-    libasound2 \
     xfonts-base \
     xfonts-terminus \
     fonts-noto \
@@ -52,7 +57,15 @@ PACKAGE_LIST="
     nano \
     locales"
 
+# Packages to attempt to install if essential tools are missing (ie: vncpasswd).
+# This is useful, at least, for Ubuntu 22.04 (jammy)
+package_list_additional="
+    tigervnc-tools"
+
 set -e
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
 
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
@@ -63,7 +76,7 @@ fi
 if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
     USERNAME=""
     POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-    for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
         if id -u ${CURRENT_USER} > /dev/null 2>&1; then
             USERNAME=${CURRENT_USER}
             break
@@ -75,38 +88,132 @@ if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
 elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
     USERNAME=root
 fi
+# Add default Fluxbox config files if none are already present
+fluxbox_apps="$(cat \
+<< 'EOF'
+[transient] (role=GtkFileChooserDialog)
+  [Dimensions]	{70% 70%}
+  [Position]	(CENTER)	{0 0}
+[end]
+EOF
+)"
 
-# Function to run apt-get if needed
-apt-get-update-if-needed()
-{
-    if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
-        echo "Running apt-get update..."
-        apt-get update
-    else
-        echo "Skipping apt-get update."
+fluxbox_init="$(cat \
+<< 'EOF'
+session.configVersion:	13
+session.menuFile:	~/.fluxbox/menu
+session.keyFile: ~/.fluxbox/keys
+session.styleFile: /usr/share/fluxbox/styles/qnx-photon
+session.screen0.workspaces: 1
+session.screen0.workspacewarping: false
+session.screen0.toolbar.widthPercent: 100
+session.screen0.strftimeFormat: %a %l:%M %p
+session.screen0.toolbar.tools: RootMenu, clock, iconbar, systemtray
+session.screen0.workspaceNames: One,
+EOF
+)"
+
+fluxbox_menu="$(cat \
+<< 'EOF'
+[begin] (  Application Menu  )
+    [exec] (File Manager) { nautilus ~ } <>
+    [exec] (Text Editor) { mousepad } <>
+    [exec] (Terminal) { tilix -w ~ -e $(readlink -f /proc/$$/exe) -il } <>
+    [exec] (Web Browser) { x-www-browser --disable-dev-shm-usage } <>
+    [submenu] (System) {}
+        [exec] (Set Resolution) { tilix -t "Set Resolution" -e bash /usr/local/bin/set-resolution } <>
+        [exec] (Edit Application Menu) { mousepad ~/.fluxbox/menu } <>
+        [exec] (Passwords and Keys) { seahorse } <>
+        [exec] (Top Processes) { tilix -t "Top" -e htop } <>
+        [exec] (Disk Utilization) { tilix -t "Disk Utilization" -e ncdu / } <>
+        [exec] (Editres) {editres} <>
+        [exec] (Xfontsel) {xfontsel} <>
+        [exec] (Xkill) {xkill} <>
+        [exec] (Xrefresh) {xrefresh} <>
+    [end]
+    [config] (Configuration)
+    [workspaces] (Workspaces)
+[end]
+EOF
+)"
+
+# Copy config files if the don't already exist
+copy_fluxbox_config() {
+    local target_dir="$1"
+    mkdir -p "${target_dir}/.fluxbox"
+    touch "${target_dir}/.Xmodmap"
+    if [ ! -e "${target_dir}/.fluxbox/apps" ]; then
+        echo "${fluxbox_apps}" > "${target_dir}/.fluxbox/apps"
+    fi
+    if [ ! -e "${target_dir}/.fluxbox/init" ]; then
+        echo "${fluxbox_init}" > "${target_dir}/.fluxbox/init"
+    fi
+    if [ ! -e "${target_dir}/.fluxbox/menu" ]; then
+        echo "${fluxbox_menu}" > "${target_dir}/.fluxbox/menu"
     fi
 }
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+##########################
+#  Install starts here   #
+##########################
 
 # Ensure apt is in non-interactive to avoid prompts
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get-update-if-needed
+apt_get_update
 
-# On older Ubuntu, Tilix is in a PPA. On Debian Strech, its in backports
+# On older Ubuntu, Tilix is in a PPA. on Debian stretch its in backports.
 if [[ -z $(apt-cache --names-only search ^tilix$) ]]; then
-    apt-get install -y --no-install-recommends lsb-release
-    if [ "$(lsb_release -is)" = "Ubuntu" ]; then
-        apt-get install -y --no-install-recommends apt-transport-https software-properties-common
+    . /etc/os-release
+    if [ "${ID}" = "ubuntu" ]; then
+        check_packages apt-transport-https software-properties-common
         add-apt-repository -y ppa:webupd8team/terminix
-    else
-        echo "deb http://deb.debian.org/debian $(lsb_release -cs)-backports main" > /etc/apt/sources.list.d/$(lsb_release -cs)-backports.list
+    elif [ "${VERSION_CODENAME}" = "stretch" ]; then
+        echo "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list
     fi
     apt-get update
+    if [[ -z $(apt-cache --names-only search ^tilix$) ]]; then
+        echo "(!) WARNING: Tilix not available on ${ID} ${VERSION_CODENAME} architecture $(uname -m). Skipping."
+    else
+        package_list="${package_list} tilix"
+    fi
+else
+    package_list="${package_list} tilix"
 fi
 
 # Install X11, fluxbox and VS Code dependencies
-if ! dpkg -s ${PACKAGE_LIST} > /dev/null 2>&1; then
-    apt-get -y install --no-install-recommends ${PACKAGE_LIST}
+check_packages ${package_list}
+
+# if Ubuntu-24.04, noble(numbat) found, then will install libasound2-dev instead of libasound2.
+# this change is temporary, https://packages.ubuntu.com/noble/libasound2 will switch to libasound2 once it is available for Ubuntu-24.04, noble(numbat)
+. /etc/os-release
+if [ "${ID}" = "ubuntu" ] && [ "${VERSION_CODENAME}" = "noble" ]; then
+    echo "Ubuntu 24.04, Noble(Numbat) detected. Installing libasound2-dev package..."
+    check_packages "libasound2-dev"
+else 
+    check_packages "libasound2"
+fi
+
+# On newer versions of Ubuntu (22.04), 
+# we need an additional package that isn't provided in earlier versions
+if ! type vncpasswd > /dev/null 2>&1; then
+    check_packages ${package_list_additional}
 fi
 
 # Install Emoji font if available in distro - Available in Debian 10+, Ubuntu 18.04+
@@ -130,8 +237,6 @@ if [ ! -d "/usr/share/fonts/truetype/cascadia" ]; then
 fi
 
 # Install noVNC
-NOVNC_VERSION=1.2.0
-WEBSOCKETIFY_VERSION=0.9.0
 if [ "${INSTALL_NOVNC}" = "true" ] && [ ! -d "/usr/local/novnc" ]; then
     mkdir -p /usr/local/novnc
     curl -sSL https://github.com/novnc/noVNC/archive/v${NOVNC_VERSION}.zip -o /tmp/novnc-install.zip
@@ -142,30 +247,16 @@ if [ "${INSTALL_NOVNC}" = "true" ] && [ ! -d "/usr/local/novnc" ]; then
     ln -s /usr/local/novnc/websockify-${WEBSOCKETIFY_VERSION} /usr/local/novnc/noVNC-${NOVNC_VERSION}/utils/websockify
     rm -f /tmp/websockify-install.zip /tmp/novnc-install.zip
 
-    # noVNC works best with Python 2 right now. Install the right package and use it.
-    if  [[ -z $(apt-cache --names-only search '^python2-minimal$') ]]; then
-        NOVNC_PYTHON_PACKAGE="python-minimal"
-    else
-        NOVNC_PYTHON_PACKAGE="python2-minimal"
-    fi
-    # Distros all have python-numpy for python2 right now, but future proof
-    if [[ -z $(apt-cache --names-only search '^python2-numpy$') ]]; then
-        NOVNC_NUMPY_PACKAGE="python-numpy"
-    else
-        NOVNC_NUMPY_PACKAGE="python2-numpy"
-    fi
-    if ! dpkg -s ${NOVNC_PYTHON_PACKAGE} ${NOVNC_NUMPY_PACKAGE} > /dev/null 2>&1; then
-        apt-get -y install --no-install-recommends ${NOVNC_PYTHON_PACKAGE} ${NOVNC_NUMPY_PACKAGE}
-    fi
-    sed -i -E 's/^python /python2 /' /usr/local/novnc/websockify-${WEBSOCKETIFY_VERSION}/run
+    # Install noVNC dependencies and use them.
+    check_packages python3-minimal python3-numpy
+    sed -i -E 's/^python /python3 /' /usr/local/novnc/websockify-${WEBSOCKETIFY_VERSION}/run
 fi
 
 # Set up folders for scripts and init files
-mkdir -p /var/run/dbus /usr/local/etc/vscode-dev-containers/ /root/.fluxbox
+mkdir -p /var/run/dbus /usr/local/etc/vscode-dev-containers/
 
 # Script to change resolution of desktop
-tee /usr/local/bin/set-resolution > /dev/null \
-<< EOF
+cat << EOF > /usr/local/bin/set-resolution
 #!/bin/bash
 RESOLUTION=\${1:-\${VNC_RESOLUTION:-1920x1080}}
 DPI=\${2:-\${VNC_DPI:-96}}
@@ -202,21 +293,27 @@ echo -e "\nSuccess!\n"
 EOF
 
 # Container ENTRYPOINT script
-tee /usr/local/share/desktop-init.sh > /dev/null \
-<< EOF
- #!/bin/bash
+cat << EOF > /usr/local/share/desktop-init.sh
+#!/bin/bash
 
-USERNAME=${USERNAME}
+user_name="${USERNAME}"
+group_name="$(id -gn ${USERNAME})"
 LOG=/tmp/container-init.log
+
+export DBUS_SESSION_BUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS:-"autolaunch:"}"
+export DISPLAY="${DISPLAY:-:1}"
+export VNC_RESOLUTION="${VNC_RESOLUTION:-1440x768x16}" 
+export LANG="${LANG:-"en_US.UTF-8"}"
+export LANGUAGE="${LANGUAGE:-"en_US.UTF-8"}"
 
 # Execute the command it not already running
 startInBackgroundIfNotRunning()
 {
     log "Starting \$1."
     echo -e "\n** \$(date) **" | sudoIf tee -a /tmp/\$1.log > /dev/null
-    if ! pidof \$1 > /dev/null; then
+    if ! pgrep -x \$1 > /dev/null; then
         keepRunningInBackground "\$@"
-        while ! pidof \$1 > /dev/null; do
+        while ! pgrep -x \$1 > /dev/null; do
             sleep 1
         done
         log "\$1 started."
@@ -245,8 +342,8 @@ sudoIf()
 # Use sudo to run as non-root user if not already running
 sudoUserIf()
 {
-    if [ "\$(id -u)" -eq 0 ] && [ "\${USERNAME}" != "root" ]; then
-        sudo -u \${USERNAME} "\$@"
+    if [ "\$(id -u)" -eq 0 ] && [ "\${user_name}" != "root" ]; then
+        sudo -u \${user_name} "\$@"
     else
         "\$@"
     fi
@@ -262,25 +359,35 @@ log "** SCRIPT START **"
 
 # Start dbus.
 log 'Running "/etc/init.d/dbus start".'
-if [ -f "/var/run/dbus/pid" ] && ! pidof dbus-daemon  > /dev/null; then
+if [ -f "/var/run/dbus/pid" ] && ! pgrep -x dbus-daemon  > /dev/null; then
     sudoIf rm -f /var/run/dbus/pid
 fi
 sudoIf /etc/init.d/dbus start 2>&1 | sudoIf tee -a /tmp/dbus-daemon-system.log > /dev/null
-while ! pidof dbus-daemon > /dev/null; do
+while ! pgrep -x dbus-daemon > /dev/null; do
     sleep 1
 done
 
 # Startup tigervnc server and fluxbox
-sudo rm -rf /tmp/.X11-unix /tmp/.X*-lock
+sudoIf rm -rf /tmp/.X11-unix /tmp/.X*-lock
 mkdir -p /tmp/.X11-unix
 sudoIf chmod 1777 /tmp/.X11-unix
-sudoIf chown root:\${USERNAME} /tmp/.X11-unix
+sudoIf chown root:\${group_name} /tmp/.X11-unix
 if [ "\$(echo "\${VNC_RESOLUTION}" | tr -cd 'x' | wc -c)" = "1" ]; then VNC_RESOLUTION=\${VNC_RESOLUTION}x16; fi
-startInBackgroundIfNotRunning "Xtigervnc" sudoUserIf "tigervncserver -screen \${DISPLAY:-:1} \${VNC_RESOLUTION:-1440x768x16} -rfbport \${VNC_PORT:-5901} -dpi \${VNC_DPI:-96} -localhost -desktop fluxbox -fg -passwd /usr/local/etc/vscode-dev-containers/vnc-passwd"
+screen_geometry="\${VNC_RESOLUTION%*x*}"
+screen_depth="\${VNC_RESOLUTION##*x}"
 
-# Spin up noVNC if installed and not runnning.
+# Check if VNC_PASSWORD is set and use the appropriate command
+common_options="tigervncserver \${DISPLAY} -geometry \${screen_geometry} -depth \${screen_depth} -rfbport ${VNC_PORT} -dpi \${VNC_DPI:-96} -localhost -desktop fluxbox -fg"
+
+if [ -n "\${VNC_PASSWORD+x}" ]; then
+    startInBackgroundIfNotRunning "Xtigervnc" sudoUserIf "\${common_options} -passwd /usr/local/etc/vscode-dev-containers/vnc-passwd"
+else
+    startInBackgroundIfNotRunning "Xtigervnc" sudoUserIf "\${common_options} -SecurityTypes None"
+fi
+
+# Spin up noVNC if installed and not running.
 if [ -d "/usr/local/novnc" ] && [ "\$(ps -ef | grep /usr/local/novnc/noVNC*/utils/launch.sh | grep -v grep)" = "" ]; then
-    keepRunningInBackground "noVNC" sudoIf "/usr/local/novnc/noVNC*/utils/launch.sh --listen \${NOVNC_PORT:-6080} --vnc localhost:\${VNC_PORT:-5901}"
+    keepRunningInBackground "noVNC" sudoIf "/usr/local/novnc/noVNC*/utils/launch.sh --listen ${NOVNC_PORT} --vnc localhost:${VNC_PORT}"
     log "noVNC started."
 else
     log "noVNC is already running or not installed."
@@ -292,61 +399,39 @@ exec "\$@"
 log "** SCRIPT EXIT **"
 EOF
 
-echo "${VNC_PASSWORD}" | vncpasswd -f > /usr/local/etc/vscode-dev-containers/vnc-passwd
-touch /root/.Xmodmap
+if [ -n "${VNC_PASSWORD+x}" ]; then
+    echo "${VNC_PASSWORD}" | vncpasswd -f > /usr/local/etc/vscode-dev-containers/vnc-passwd
+fi
 chmod +x /usr/local/share/desktop-init.sh /usr/local/bin/set-resolution
 
-tee /root/.fluxbox/apps > /dev/null \
-<<EOF
-[transient] (role=GtkFileChooserDialog)
-  [Dimensions]	{70% 70%}
-  [Position]	(CENTER)	{0 0}
-[end]
-EOF
-
-tee /root/.fluxbox/init > /dev/null \
-<<EOF
-session.configVersion:	13
-session.menuFile:	~/.fluxbox/menu
-session.keyFile: ~/.fluxbox/keys
-session.styleFile: /usr/share/fluxbox/styles/qnx-photon
-session.screen0.workspaces: 1
-session.screen0.workspacewarping: false
-session.screen0.toolbar.widthPercent: 100
-session.screen0.strftimeFormat: %a %l:%M %p
-session.screen0.toolbar.tools: RootMenu, clock, iconbar, systemtray
-session.screen0.workspaceNames: One,
-EOF
-
-tee /root/.fluxbox/menu > /dev/null \
-<<EOF
-[begin] (  Application Menu  )
-    [exec] (File Manager) { nautilus ~ } <>
-    [exec] (Text Editor) { mousepad } <>
-    [exec] (Terminal) { tilix -w ~ -e $(readlink -f /proc/$$/exe) -il } <>
-    [exec] (Web Browser) { x-www-browser --disable-dev-shm-usage } <>
-    [submenu] (System) {}
-        [exec] (Set Resolution) { tilix -t "Set Resolution" -e bash /usr/local/bin/set-resolution } <>
-        [exec] (Edit Application Menu) { mousepad ~/.fluxbox/menu } <>
-        [exec] (Passwords and Keys) { seahorse } <>
-        [exec] (Top Processes) { tilix -t "Top" -e htop } <>
-        [exec] (Disk Utilization) { tilix -t "Disk Utilization" -e ncdu / } <>
-        [exec] (Editres) {editres} <>
-        [exec] (Xfontsel) {xfontsel} <>
-        [exec] (Xkill) {xkill} <>
-        [exec] (Xrefresh) {xrefresh} <>
-    [end]
-    [config] (Configuration)
-    [workspaces] (Workspaces)
-[end]
-EOF
-
-# Set up non-root user (if one exists)
+# Set up fluxbox config
+copy_fluxbox_config "/root"
 if [ "${USERNAME}" != "root" ]; then
-    touch /home/${USERNAME}/.Xmodmap
-    cp -R /root/.fluxbox /home/${USERNAME}
-    chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/.Xmodmap /home/${USERNAME}/.fluxbox
-    chown ${USERNAME}:root /usr/local/share/desktop-init.sh /usr/local/bin/set-resolution /usr/local/etc/vscode-dev-containers/vnc-passwd
+    copy_fluxbox_config "/home/${USERNAME}"
+    chown -R ${USERNAME} /home/${USERNAME}/.Xmodmap /home/${USERNAME}/.fluxbox
 fi
 
-echo "Done!"
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+# Determine the message based on whether VNC_PASSWORD is set
+if [ -n "${VNC_PASSWORD+x}" ]; then
+    PASSWORD_MESSAGE="In both cases, use the password \"${VNC_PASSWORD}\" when connecting"
+else
+    PASSWORD_MESSAGE="In both cases, no password is required."
+fi
+
+# Display the message
+cat << EOF
+
+
+You now have a working desktop! Connect to in one of the following ways:
+
+- Forward port ${NOVNC_PORT} and use a web browser to start the noVNC client (recommended)
+- Forward port ${VNC_PORT} using VS Code client and connect using a VNC Viewer
+
+${PASSWORD_MESSAGE}
+
+(*) Done!
+
+EOF
